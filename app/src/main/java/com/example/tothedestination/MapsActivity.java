d8f9138cef02db6c3f2270daaa0314c643daffda
ng7897 <naamagr13@gmail.com>
2025-06-08 21:10:09 +0300

    add activity

--- app/src/main/java/com/example/tothedestination/MapsActivity.java
+++ app/src/main/java/com/example/tothedestination/MapsActivity.java
@@ -26,39 +26,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //ViewBinding זה במקום findViewById, ViewBinding יוצר אובייקט שמייצג את כל הרכיבים שבxml
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        //מציג את הlayout על המסך
         setContentView(binding.getRoot());
 
+        //קבלת רשימת האטרקציות שהמשתמש סימן בcheckBox
         Intent intent = getIntent();
         attList = (ArrayList<Attraction>) intent.getSerializableExtra("attList");
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //איתור רכיב המפה לפי הid בxml(map) והמרתו לSupportMapFragment
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        //טוען את המפה בצורה שלא חוסמת את המשך הקוד, טעינת המפה לוקחת זמן בגלל שהיא מהאיטרנט ולכן מבקשים להתעדכן כשזה מוכן
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        //סימון נמל התעופה והזזת המצלמה
         LatLng airtport1 = new LatLng(getIntent().getDoubleExtra("coordinatesX", 0), getIntent().getDoubleExtra("coordinatesY", 0));
-        //LatLng airport = getIntent().getParcelableExtra("airport");
-
         mMap.addMarker(new MarkerOptions().position(airtport1).title("Marker in airtport"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(airtport1));
 
-        // Add markers for each attraction
+        // סימון כל אטרקציה שקיבלנו
         for (Attraction attraction : attList) {
             LatLng location = new LatLng(attraction.getCoordinatesX(), attraction.getCoordinatesY());
             mMap.addMarker(new MarkerOptions()
---
+++ app/src/main/java/com/example/tothedestination/informationProgrammerMain.java
@@ -0,0 +1,18 @@
+package com.example.tothedestination;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class informationProgrammerMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.information_programmer);
+
+    }
+}
